0c093b4fd7c39830cdb843036b871a59bba9c7b4
Davidcito12 <92931033+Davidcito12@users.noreply.github.com>
2022-04-02 19:47:04 -0500

    Intento 2

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,24 @@
 package classroom;
 
 public enum Tipo {
-	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
+	DISCIPLINAR(10,"Disciplinar"),FUNDAMENTACION(20,"Fundamentacion"),ELECTIVA(30,"Electiva");
 	
 	private int codigo;
 	private String nombre;
-	public Tipo(int codigo,String nombre) {
+	private Tipo(int codigo,String nombre) {
+		this.setCodigo(codigo);
+		this.setNombre(nombre);
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
 		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 }
